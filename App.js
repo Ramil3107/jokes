3c210f49a4bab2eec4fb956314fad596e4f0a3d8
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-16 23:29:06 +0300

    init

---
+++ App.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          Hello World!
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
