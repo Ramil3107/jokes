5a02e01d0f40d3b3682c23e632df40600c2b109c
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-17 02:46:56 +0300

    add icons and styles to tab bar

--- src/Stack.js
+++ src/Stack.js
@@ -2,6 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Today } from './screens/Today';
 import { History } from './screens/History';
+import TodayIconSvg from './assets/icons/TodayIconSvg';
+import HistoryIconSvg from './assets/icons/HistoryIconSvg';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -9,20 +12,29 @@ export const Stack = () => {
     return (
         <NavigationContainer>
             <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarStyle: { height: 102, borderTopWidth: 1 },
+                screenOptions={{
+                    tabBarStyle: { height: 102, borderTopWidth: 1, paddingHorizontal: 100, paddingBottom: 40 },
+                    tabBarIconStyle: { marginBottom: -10 },
                     tabBarActiveTintColor: "#9763FF",
-                    tabBarInactiveTintColor: "#C1C3C6"
-                })}
+                    tabBarInactiveTintColor: "#C1C3C6",
+                    headerShown: false,
+                    tabBarLabelStyle: { fontSize: 12, fontWeight: "600" }
+                }}
                 sceneContainerStyle={{ backgroundColor: 'white', borderTopColor: "#E6E6E6" }}
             >
                 <Tab.Screen
                     name="Today"
                     component={Today}
                     options={{
-                        headerShown: false,
+                        tabBarIcon: ({ color }) => <TodayIconSvg width={28} height={28} stroke={color} />
                     }} />
-                <Tab.Screen name="History" component={History} options={{ headerShown: false }} />
+                <Tab.Screen
+                    name="History"
+                    component={History}
+                    options={{
+                        tabBarIcon: ({ color }) => <HistoryIconSvg width={28} height={28} stroke={color} />
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     )
