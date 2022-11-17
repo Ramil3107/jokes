399c4d6c81583c79fc8e346754a5f7eb751f7069
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-18 01:19:57 +0300

    create custom hook async storage

---
+++ src/hooks/useAsyncStorage.js
@@ -0,0 +1,24 @@
+import AsyncStorage from "@react-native-community/async-storage";
+import { useEffect, useState } from "react";
+
+export function useAsyncStorage() {
+
+    async function getItem(key) {
+        try {
+            const item = await AsyncStorage.getItem(key)
+            return item
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function setItem(key, value) {
+        try {
+            const item = await AsyncStorage.setItem(key, value)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return { getItem, setItem }
+}
