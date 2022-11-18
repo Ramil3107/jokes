c79bbc84b347e0ef01ba03421f9982030c9dcdcf
Ramil Abbasov <abbasov.ram3107@gmail.com>
2022-11-18 16:30:55 +0300

    add comments to action file

--- src/screens/today/actions/actions.js
+++ src/screens/today/actions/actions.js
@@ -16,19 +16,32 @@ export const setAsyncStorage = async (setJoke) => {
     // removeItem(CURRENT_JOKE)
     // removeItem(JOKES_HISTORY)
 
-    const joke = await jokesAPI.getProgrammingJoke()
-    const fullJoke = {
-        text: joke,
-        isFavourite: false
-    }
+    const date = await getItem(DATE_KEY)   // get and parse date value from async storage
+    const parsedDate = parseJSON(date)    
 
-    const date = await getItem(DATE_KEY)
-    const parsedDate = parseJSON(date)
+    if ((date === null) || !isToday(parsedDate)) {    //  if current date value null or not today date
 
-    if ((date === null) || !isToday(parsedDate)) {
-        setItem(DATE_KEY, today)
-        setItem(CURRENT_JOKE, fullJoke)
-        setJoke(fullJoke)
+        const joke = await jokesAPI.getProgrammingJoke()   // fetch joke from api and prepare object structure
+        const fullJoke = {       
+            text: joke,
+            isFavourite: false
+        }
+
+        setItem(DATE_KEY, today)  // set current date value to storage
+        setItem(CURRENT_JOKE, fullJoke)  // set daily joke to storage
+        setJoke(fullJoke) // set daily joke to local state
+
+        const jokeHistory = await getItem(JOKES_HISTORY)  
+
+        if (jokeHistory === null) {                 // set new history to storage if storage hisstory == null
+            const newHistory = [{ ...fullJoke }]
+            setItem(JOKES_HISTORY, newHistory)
+        }
+        else {  // update storage history array
+            const updatedHistory = [...jokeHistory, { ...fullJoke }]
+            setItem(JOKES_HISTORY, updatedHistory)
+        }
+
     } else if (isToday(parsedDate)) {
         const currentJoke = await getItem(CURRENT_JOKE)
         setJoke(currentJoke)
